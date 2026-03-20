ef47f35d54b34e5cfe2cd82170e0cca8f5735bef
advaitb2028 <bhowmik1@purdue.edu>
2026-03-20 19:23:15 -0400

    Update server.py

--- server.py
+++ server.py
@@ -133,7 +133,7 @@ def analyze():
 
     fen += f"{move_count} {half_move_count}"
 
-    engine = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish-macos-m1-apple-silicon")
+    engine = chess.engine.SimpleEngine.popen_uci("/usr/bin/stockfish")
     board = chess.Board(fen)
 
     info = engine.analyse(board, chess.engine.Limit(time=0.1))
